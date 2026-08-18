1519cb86c5f208033ca379f19adbc829c2c67c34
rq2b <sankalysenko@gmail.com>
2026-08-18 16:33:25 +0300

    update: restart -> reboot

--- config/shell/.bashrc.d/reboot.sh
+++ config/shell/.bashrc.d/reboot.sh
@@ -6,3 +6,8 @@ restart() {
         echo "Cancelled."
     fi
 }
+
+reboot() {
+    read -p "Type yes to reboot: " confirm
+    [ "$confirm" = "yes" ] && sudo systemctl reboot || echo "Cancelled."
+}
